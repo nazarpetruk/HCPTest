51f02a2c2624159d31e7598bf8e0afb8fa148994
nazarPetruk <nazarpetruk15@gmail.com>
2020-02-12 10:01:43 +0100

    Initial Commit

---
+++ HCPTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HCPTest
+//
+//  Created by Nazar Petruk on 12/02/2020.
+//  Copyright © 2020 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
